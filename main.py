756472c488a432539b743b2089326c3153e1e404
Kira <loskiros.music@gmail.com>
2018-06-02 21:25:09 +0200

    update

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ logger = logging.getLogger(__name__)
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
-                    text="I'm a bot, please talk to me!")
+                    text="I am your personal planning assistant, as reposonsible and nice as C3PO! (and also a bit dumb, just to balance things, you know)")
 
 def hello(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
-                    text='Hello {}'.format(update.message.from_user.first_name))
+                    text='Let me remember your plans for you, {}. You can send me a todo, and I will send you a list of todos back'.format(update.message.from_user.first_name))
 
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text='Help!')
@@ -27,7 +27,7 @@ def echo(bot, update):
 
 def caps(bot, update, args):
     text_caps = ' '.join(args).upper()
-    bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
+    bot.sendMessage(chat_id=update.mes--sage.chat_id, text=text_caps)
 
 
 # Helpers
